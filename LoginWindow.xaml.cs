f1d730c54fd5052899eae0f61c862236a022b4bf
M00nl1ght07 <denis72728282@yandex.ru>
2025-02-25 18:56:19 +0300

    окно авторизации

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServiceWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            
+            // Добавляем обработчики событий для подсказки пароля
+            PasswordBox.GotFocus += (s, e) => PasswordHint.Visibility = Visibility.Collapsed;
+            PasswordBox.LostFocus += (s, e) => 
+            {
+                if (string.IsNullOrEmpty(PasswordBox.Password))
+                    PasswordHint.Visibility = Visibility.Visible;
+            };
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
